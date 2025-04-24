8359d007b7540ca6b41fd6d2debb6a315feb8cee
Abdelrahman-Yaser <abdooy640@gmail.com>
2025-04-24 16:31:21 +0200

    2s

--- app/axios/axios.js
+++ app/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://edu-backend-git-main-abdelrahman-yasers-projects.vercel.app",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
